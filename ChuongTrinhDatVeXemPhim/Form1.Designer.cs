f39cbb7d0a7e1772d58fbe9f622df779900eed7d
hoangdev <nvhhoang286@gmail.com>
2023-10-01 21:22:42 +0700

    first

---
+++ ChuongTrinhDatVeXemPhim/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace ChuongTrinhDatVeXemPhim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            tableLayoutPanelGhe = new TableLayoutPanel();
+            panel2 = new Panel();
+            label8 = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            btnThanhTan = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(963, 34);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(963, 34);
+            label1.TabIndex = 0;
+            label1.Text = "NGƯỜI / GHẾ";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Dock = DockStyle.Top;
+            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.Location = new Point(0, 34);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(963, 39);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanelGhe
+            // 
+            tableLayoutPanelGhe.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanelGhe.ColumnCount = 13;
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266224F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.69266272F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.6903553F));
+            tableLayoutPanelGhe.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.6903553F));
+            tableLayoutPanelGhe.Location = new Point(154, 114);
+            tableLayoutPanelGhe.Name = "tableLayoutPanelGhe";
+            tableLayoutPanelGhe.RowCount = 10;
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanelGhe.Size = new Size(674, 324);
+            tableLayoutPanelGhe.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label8);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 73);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(963, 41);
+            panel2.TabIndex = 2;
+            // 
+            // label8
+            // 
+            label8.Dock = DockStyle.Fill;
+            label8.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(0, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(963, 41);
+            label8.TabIndex = 0;
+            label8.Text = "SCREEN";
+            label8.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Left;
+            panel3.Location = new Point(0, 114);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(154, 424);
+            panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            panel4.Dock = DockStyle.Right;
+            panel4.Location = new Point(828, 114);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(135, 424);
+            panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(btnThanhTan);
+            panel5.Controls.Add(label5);
+            panel5.Controls.Add(label6);
+            panel5.Controls.Add(label7);
+            panel5.Controls.Add(label4);
+            panel5.Controls.Add(label3);
+            panel5.Controls.Add(label2);
+            panel5.Dock = DockStyle.Bottom;
+            panel5.Location = new Point(154, 438);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(674, 100);
+            panel5.TabIndex = 5;
+            // 
+            // btnThanhTan
+            // 
+            btnThanhTan.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnThanhTan.Location = new Point(512, 35);
+            btnThanhTan.Name = "btnThanhTan";
+            btnThanhTan.Size = new Size(113, 36);
+            btnThanhTan.TabIndex = 4;
+            btnThanhTan.Text = "Thanh toán";
+            btnThanhTan.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(318, 14);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 1;
+            label5.Text = "label2";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(318, 71);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 2;
+            label6.Text = "label2";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(318, 43);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 3;
+            label7.Text = "label7";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(110, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 0;
+            label4.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(110, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 0;
+            label3.Text = "label2";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(110, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 0;
+            label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.LightYellow;
+            ClientSize = new Size(963, 538);
+            Controls.Add(tableLayoutPanelGhe);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TableLayoutPanel tableLayoutPanelGhe;
+        private Panel panel2;
+        private Label label8;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Button btnThanhTan;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+    }
+}
---
+++ ChuongTrinhDatVeXemPhim/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection.Emit;
+
+namespace ChuongTrinhDatVeXemPhim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            loadSeet();
+        }
+
+        private void loadSeet()
+        {
+            for (char row = 'A'; row <= 'K'; row++)
+            {
+                if (row == 'I')
+                {
+                    continue;
+                }
+                int totalSeatsInRow = (row == 'K') ? 15 : 13;
+
+                for (int seatNum = 1; seatNum <= totalSeatsInRow; seatNum++)
+                {
+                    string seatName = row + seatNum.ToString(); // Tạo tên ghế, ví dụ: "A1", "B2",...
+
+                    Button seatButton = new Button();
+                    seatButton.Name = seatName;
+                    seatButton.Text = seatName;
+                    seatButton.Width = 50; // Điều chỉnh kích thước của ghế tùy ý
+                    seatButton.Height = 30;
+                    seatButton.Click += SeatButton_Click; // Gắn sự kiện click cho ghế
+
+                    tableLayoutPanelGhe.Controls.Add(seatButton); // Thêm ghế vào TableLayoutPanel
+                }
+            }
+        }
+        //thực hiện khi click vào ghế ở đây
+        private void SeatButton_Click(object? sender, EventArgs e)
+        {
+
+        }
+
+        /*
+         * 1. checkout qua nhánh khác
+         * 2. đổi màu các ghế + làm lại các label ở dưới
+         * 3. thực hiện các thao tác khi click vào các ghế
+        */
+    }
+}
